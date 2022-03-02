352d0da9669bb32cacf59dcda2ecf1b8c3527bd0
Wemmons831 <69495081+Wemmons831@users.noreply.github.com>
2022-03-02 15:25:53 -0500

    fixed unused time

--- src/impl/kernal/mainc.cpp
+++ src/impl/kernal/mainc.cpp
@@ -1,5 +1,5 @@
 #include "../include/print.h"
-#include "../include/time.h"
+
 #include <stdint.h>
 
 
@@ -8,8 +8,7 @@ extern "C" void kernel_main() {
     
     print_set_color(PRINT_COLOR_BLACK, PRINT_COLOR_WHITE);
     set_screen_color(PRINT_COLOR_WHITE);
-    read_rtc();
-    print_str(reinterpret_cast<char*>(second));
+    print_str("booted :)");
     
     
 
